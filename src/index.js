26d4ab77b3d276666c99eab020aed158ff4b0353
Morgan Kartchner <mkartchner994@gmail.com>
2019-09-29 11:22:32 -0600

    Upgrade to use latest version of kcd-scripts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import Dayzed from './dayzed';
-
-/*
- * Fix importing in typescript after rollup compilation
- * https://github.com/rollup/rollup/issues/1156
- * https://github.com/Microsoft/TypeScript/issues/13017#issuecomment-268657860
- */
-Dayzed.default = Dayzed;
-
-export default Dayzed;
+export { default, useDayzed } from './dayzed';
